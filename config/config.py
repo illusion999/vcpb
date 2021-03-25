2443be6d3eb5df82dc28d7e9693af53db7d0185e
illusion999 <79356567+illusion999@users.noreply.github.com>
2021-03-26 04:13:49 +0530

    Update and rename sample_config.py to config.py

---
+++ config/config.py
@@ -0,0 +1,17 @@
+# You can copy this file to config.py and add your info
+
+# Get API ID and HASH from https://my.telegram.org
+API_ID = 3277132
+API_HASH = "50ad2ffad01bf10049638526009661a8"
+
+# Get this from @Botfather
+BOT_TOKEN = "1735820251:AAF4dEIEWGKZ42D_Ioc3fQeKr48FLWEWzJw"
+
+# The IDs of the users which can stream, skip, pause and change volume
+SUDO_USERS = [
+    1410616929,
+    1678331806,
+    1678331806
+]
+
+CHAT_ID = -1001162414271  # ID of the group where the bot streams
